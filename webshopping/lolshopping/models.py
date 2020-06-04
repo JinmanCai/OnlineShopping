7b5a84e3c571622fc686151a72d2e430c86009bb
jinmanc <jinmanc@yahoo.com>
2020-06-04 13:51:55 -0500

    set up models and retrieve data from DB

--- webshopping/lolshopping/models.py
+++ webshopping/lolshopping/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Champions(models.Model):
+    name = models.CharField(max_length=30)
+    role = models.CharField(max_length=30)
+    price = models.DecimalField(decimal_places = 0, max_digits=2)
+    picture = models.ImageField(upload_to='champic/')
