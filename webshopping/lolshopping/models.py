4455dec67cda49e98b7661cd8b2d7aa403125ac3
jinmanc <jinmanc@yahoo.com>
2020-06-05 16:44:11 -0500

    display all the champ in rows and columns with an add to cart button

--- webshopping/lolshopping/models.py
+++ webshopping/lolshopping/models.py
@@ -6,3 +6,5 @@ class Champions(models.Model):
     role = models.CharField(max_length=30)
     price = models.DecimalField(decimal_places = 0, max_digits=2)
     picture = models.ImageField(upload_to='champic/')
+    def __str__(self):
+        return self.name
