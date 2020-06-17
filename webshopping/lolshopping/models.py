4661fc3dd4f8bfea932832806be1bc9d6e300afd
jinmanc <jinmanc@yahoo.com>
2020-06-16 22:47:29 -0500

    fixed filter on nav bar

--- webshopping/lolshopping/models.py
+++ webshopping/lolshopping/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+class Account(AbstractBaseUser):
+    email = models.EmailField(verbose_name = 'email', max_length = 60, unique=True)
+    username = models.CharField(max_length = 30, unique=True)
 
 
 
